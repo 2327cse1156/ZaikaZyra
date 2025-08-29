602ab0df779a6fa1c641c7c07839b3a159fc63b0
2327cse1156 <anshkaushal92@gmail.com>
2025-08-29 19:50:18 +0530

    user routes created

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
 import cors from "cors";
 
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   connectDB();
